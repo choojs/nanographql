179f95bcd2e233dada4a0e826945c7543281050d
Calin <calinoiu.alexandru@agilefreaks.com>
2017-09-01 08:31:05 +0200

    Adds mutations support

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = nanogql
 
-var getOpname = /query ([\w\d-_]+)? ?\(.*?\)? \{/
+var getOpname = /(query|mutation) ?([\w\d-_]+)? ?\(.*?\)? \{/
 
 function nanogql (str) {
   str = Array.isArray(str) ? str.join('') : str
@@ -8,7 +8,7 @@ function nanogql (str) {
   return function (variables) {
     var data = { query: str }
     if (variables) data.variables = JSON.stringify(variables)
-    if (name) data.operationName = name[1]
+    if (name) data.operationName = name[2]
     return data
   }
 }
