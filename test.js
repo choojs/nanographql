dfc8d8c25911a2451c4a75c3f2bbc101154ddd8a
Karpich Dmitry <karpich@gollard.ru>
2019-01-23 18:35:15 +0300

    * rewrite tests

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
-var spok = require('spok')
 var tape = require('tape')
 var gql = require('./')
 
-tape('should create a query', function (assert) {
+tape('should create a query', function (test) {
   var query = gql`
     query($number_of_repos:Int!) {
       viewer {
@@ -15,17 +14,17 @@ tape('should create a query', function (assert) {
       }
     }
   `
-
   var variables = { number_of_repos: 3 }
   var data = query(variables)
-  spok(assert, JSON.parse(data), {
-    query: spok.string,
-    variables: JSON.stringify(variables)
-  })
-  assert.end()
+  var parsedData = JSON.parse(data)
+
+  test.plan(2)
+  test.ok(typeof parsedData.query === 'string', 'query should be a string')
+  test.equal(parsedData.variables, JSON.stringify(variables), 'variables processed as a string')
+  test.end()
 })
 
-tape('should have a name', function (assert) {
+tape('should have a name', function (test) {
   var query = gql`
     query foo ($number_of_repos:Int!) {
       viewer {
@@ -38,18 +37,18 @@ tape('should have a name', function (assert) {
       }
     }
   `
-
   var variables = { number_of_repos: 3 }
   var data = query(variables)
-  spok(assert, JSON.parse(data), {
-    query: spok.string,
-    operationName: 'foo',
-    variables: JSON.stringify(variables)
-  })
-  assert.end()
+  var parsedData = JSON.parse(data)
+
+  test.plan(3)
+  test.ok(typeof parsedData.query === 'string', 'query should be a string')
+  test.equal(parsedData.operationName, 'foo', 'operationName as a string')
+  test.equal(parsedData.variables, JSON.stringify(variables), 'variables processed as a string')
+  test.end()
 })
 
-tape('should have a name for mutations also', function (assert) {
+tape('should have a name for mutations also', function (test) {
   var query = gql`
     mutation CreateSomethingBig($input: Idea!) {
       createSomething(input: $input) {
@@ -57,11 +56,83 @@ tape('should have a name for mutations also', function (assert) {
       }
     }
   `
+  var data = query()
+  var parsedData = JSON.parse(data)
+
+  test.plan(2)
+  test.ok(typeof parsedData.query === 'string', 'query should be a string')
+  test.equal(parsedData.operationName, 'CreateSomethingBig', 'operationName as a string')
+  test.end()
+})
+
+tape('should have a name without args', function (test) {
+  var query = gql`
+    query foo {
+      viewer {
+        name
+        repositories {
+          nodes {
+            name
+          }
+        }
+      }
+    }
+  `
+  var data = query()
+  var parsedData = JSON.parse(data)
+
+  test.plan(2)
+  test.ok(typeof parsedData.query === 'string', 'query should be a string')
+  test.equal(parsedData.operationName, 'foo', 'operationName as a string')
+  test.end()
+})
+
+tape('should support a fragment', function (test) {
+  var fragment = `
+    fragment RepositoryFragment on Repository {
+      nodes {
+        name
+      }
+    }
+  `
+  var query = gql`
+    query foo {
+      viewer {
+        name
+        repositories {
+          ...RepositoryFragment
+        }
+      }
+    }
+    ${fragment}
+  `
 
   var data = query()
-  spok(assert, JSON.parse(data), {
-    query: spok.string,
-    operationName: 'CreateSomethingBig'
-  })
-  assert.end()
+  test.plan(2)
+  test.ok(data.indexOf('...RepositoryFragment') !== -1, 'fragment link exists')
+  test.ok(data.indexOf('fragment RepositoryFragment on Repository') !== -1, 'fragment body exists')
+  test.end()
+})
+
+tape('should not stringify variables', function (test) {
+  var query = gql`
+    query($number_of_repos:Int!) {
+      viewer {
+        name
+        repositories(last: $number_of_repos) {
+          nodes {
+            name
+          }
+        }
+      }
+    }
+  `
+  var variables = { number_of_repos: 3 }
+  var data = query(variables, true)
+  var parsedData = JSON.parse(data)
+
+  test.plan(2)
+  test.ok(typeof parsedData.query === 'string', 'query should be a string')
+  test.deepEqual(parsedData.variables, variables, 'variables processed as an object')
+  test.end()
 })
